a03e9193366f3e7a507f53fdf5f492f9ad937367
Alex <dev.alex.88@gmail.com>
2021-02-03 16:42:58 +0700

    update tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -21,23 +21,23 @@ PROJECTS = "projects"
 def create_issue(webdriver, dataset):
     issue_modal = Issue(webdriver)
 
-    @print_timing("selenium_custom_action_create_issue")
+    @print_timing("katalon_create_bdd_issue")
     def measure():
 
-        @print_timing("selenium_custom_action_create_issue:open_quick_create")
+        @print_timing("katalon_create_bdd_issue:open_quick_create")
         def sub_measure():
             issue_modal.open_create_issue_modal()
         sub_measure()
 
-        @print_timing("selenium_custom_action_create_issue:fill_and_submit_issue_form")
+        @print_timing("katalon_create_bdd_issue:fill_and_submit_issue_form")
         def sub_measure():
             issue_modal.fill_summary_create2()  # Fill summary field
-            issue_modal.fill_description_create2(rte_status)  # Fill description field
+            issue_modal.fill_description_create(rte_status)  # Fill description field
             issue_modal.assign_to_me()  # Click assign to me
             issue_modal.set_resolution()  # Set resolution if there is such field
             issue_modal.set_issue_type()  # Set issue type, use non epic type
 
-            @print_timing("selenium_custom_action_create_issue:fill_and_submit_issue_form:submit_issue_form")
+            @print_timing("katalon_create_bdd_issue:fill_and_submit_issue_form:submit_issue_form")
             def sub_sub_measure():
                 issue_modal.submit_issue()
             sub_sub_measure()
@@ -48,7 +48,7 @@ def create_issue(webdriver, dataset):
 def view_issue(webdriver, datasets):
     issue_page = Issue(webdriver, issue_key=datasets['issue_key'])
 
-    @print_timing("selenium_custom_action_view_issue")
+    @print_timing("katalon_view_bdd_issue")
     def measure():
         issue_page.go_to()
         issue_page.wait_for_page_loaded()
